e13636393e24a77c3cb637b8cae52f9ca8c06a4d
Jean-Rémy Falleri <jr.falleri@gmail.com>
2014-03-06 13:04:30 +0100

    Update app.js
    Now default port is 3000.

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var app = module.exports = express();
 */
 
 // all environments
-app.set('port', process.env.PORT || 8080);
+app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.logger('dev'));
