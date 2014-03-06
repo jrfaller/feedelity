553f36e616ee263a094d676a0342274c536b5930
Jean-Rémy Falleri <jr.falleri@gmail.com>
2014-03-06 11:32:18 +0100

    Update app.js
    Removed outdated route.

--- public/js/app.js
+++ public/js/app.js
@@ -4,11 +4,10 @@
 angular.module('feedelity', ['feedelity.filters', 'feedelity.services', 'feedelity.directives', 'ngRoute', 'ngSanitize']).
   config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
     $routeProvider.
-      when('/articles', {templateUrl: 'partial/articles', controller: ArticlesCtrl}).
       when('/articles/unread', {templateUrl: 'partial/articles', controller: ArticlesCtrl, type: 'unread'}).
       when('/articles/read', {templateUrl: 'partial/articles', controller: ArticlesCtrl, type: 'read'}).
       when('/articles/starred', {templateUrl: 'partial/articles', controller: ArticlesCtrl, type: 'starred'}).
       when('/feeds', {templateUrl: 'partial/feeds', controller: FeedsCtrl}).
-      otherwise({redirectTo: '/articles'});
+      otherwise({redirectTo: '/articles/unread'});
     $locationProvider.html5Mode(true).hashPrefix('!');
   }]);
